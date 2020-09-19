1ed088c8c7202f5ec75735e4ca00ec119e0b213f
peidcud <51078365+peidcud@users.noreply.github.com>
2020-09-19 11:26:23 +0800

    updated logo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { allProjects } from "../data";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
+import Logo from "../images/stitchr_logo.fw.png";
 
-function HomePage () {
+function HomePage() {
     return (
+        <div>
+            <img src={Logo} />
         <div id="project-list">
             {allProjects.map((projectData, key) => {
                 return <ProjectCard key={key} projectData={projectData} />;
             })}
+        </div>
         </div>
     );
 }
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -5,6 +5,7 @@ function ProjectPage () {
     return (
         <div>
             <h2>{oneProject.title}</h2>
+            <img src={oneProject.image}/>
             <h3>Created at: {oneProject.date_created}</h3>
             <h3>Status: {oneProject.is_open}</h3>
             <h3>Likes:</h3>
