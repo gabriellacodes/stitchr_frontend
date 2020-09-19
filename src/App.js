89b61eeb04613846f9505b575432f9214144bd00
peidcud <51078365+peidcud@users.noreply.github.com>
2020-09-19 10:38:00 +0800

    Created home and project pages
    Updates include:
    - Created home and project pages
    - Project cards created
    - Styling introduced for main page and project card on main page only.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./pages/HomePage";
 import ProjectPage from "./pages/ProjectPage";
+import "./App.css"
 
 export default function App() {
   return (
